3d864253d473db3db0d8714ed0668990d28bf79f
eshe0007 <eshe0007@student.monash.edu>
2022-08-04 12:52:27 +1000

    initial commit: lab 2 & 3

---
+++ lab3/FIT5032_MyCodeSnippet/FIT5032_MyCodeSnippet/HelloWorld/Hello.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+// Namespaces are like packages in Java.
+namespace FIT5032_MyCodeSnippet.HelloWorld
+{
+    public class Hello
+    {
+        // Pascal Case & Title Casing is the standard for C# Developers.
+        // => Same as captical camel casing or upper camel casing?
+        public String GetHello()
+        {
+            return "Hello World";
+        }
+    }
+}
