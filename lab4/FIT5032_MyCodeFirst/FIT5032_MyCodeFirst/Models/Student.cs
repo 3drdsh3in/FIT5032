8484c3e34930cefd0dab5a650475464a9ac11467
eshe0007 <eshe0007@student.monash.edu>
2022-08-20 15:08:28 +1000

    code first approach update

---
+++ lab4/FIT5032_MyCodeFirst/FIT5032_MyCodeFirst/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿namespace FIT5032_MyCodeFirst.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Student", Schema = "dbo")]
+    public partial class Student
+    {
+        public Student()
+        {
+            this.Units = new HashSet<Unit>();
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [ForeignKey("StudentId")]
+        public ICollection<Unit> Units { get; set; }
+    }
+}
---
+++ lab4/FIT5032_MyCodeFirst/FIT5032_MyCodeFirst/Models/Unit.cs
@@ -0,0 +1,17 @@
+﻿namespace FIT5032_MyCodeFirst.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    [Table("Unit", Schema = "dbo")]
+    public partial class Unit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StudentId { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
