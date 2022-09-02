dee38fcc83028aaf05155bfc6bf78c95247eea4b
eshe0007 <eshe0007@student.monash.edu>
2022-09-02 14:18:52 +1000

    lab 6 and extra a1 files

---
+++ assignments/portfolio/FIT5032_Assignment_Portfolio/FIT5032_Assignment_Portfolio/Models/User.sql
@@ -0,0 +1,11 @@
+﻿DROP TABLE [dbo].[tblUsers];
+GO
+
+CREATE TABLE [dbo].[tblUsers] (
+	[user_id] int IDENTITY(1,1),
+	[user_fname] VARCHAR(100),
+	[user_lname] VARCHAR(100),
+	[is_staff] [bit] NOT NULL,
+	CONSTRAINT PK_User PRIMARY KEY ([user_id])
+);
+GO
